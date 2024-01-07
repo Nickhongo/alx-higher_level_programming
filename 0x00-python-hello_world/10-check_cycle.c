6de5ec8b10d634f8caed6584121cb0cf1755015c
Nickhongo <hongonicholas0@gmail.com>
2024-01-07 12:23:03 +0300

    linked lists

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,25 +12,17 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 		return (0);
-
 	slow = list;
-	fast = list;
+	fast = list->next;
 
-	while (slow != NULL || fast != NULL)
+	while (slow != fast)
 	{
-		if (fast->next == NULL)
+		if (fast == NULL || fast->next == NULL)
 			return (0);
-
 		slow = slow->next;
 		fast = fast->next->next;
-
-		if (slow == fast)
-		{
-			return (1);
-		}
 	}
-
-	return (0);
+	return (1);
 }
