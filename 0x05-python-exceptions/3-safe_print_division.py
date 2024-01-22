4b6d01f6bd34b3d2e9fba095dd19772f8f283917
Nickhongo <hongonicholas0@gmail.com>
2024-01-22 19:29:55 +0300

    Write a function that divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    result = None
+
     try:
         result = a / b
-    except ZeroDivisionError:
-        print("Inside result: None")
-        return None
+    except (TypeError, ZeroDivisionError):
+        pass
     else:
-        print("Inside result: {}".format(result))
         return result
     finally:
-        print("Inside finally")
+        print("Inside result: {}".format(result))
+
+    return None
