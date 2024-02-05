462c869a716f1c4549ee9ea14dd9eaa0991015e1
Nickhongo <hongonicholas0@gmail.com>
2024-02-05 18:04:20 +0300

    an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains the following function:
+        MyList: defines a class that inherits from a list
+
+"""
+def MyList(list):
+    """
+     defines a class that inherits from a list
+    """
+    def print_sorted(self):
+        """
+        prints a sorted list
+        """
+        print(sorted(self))
