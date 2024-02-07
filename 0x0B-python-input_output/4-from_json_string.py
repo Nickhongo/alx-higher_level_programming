600849e4e1bb9b9f608c741e7eb076d019e82292
Nickhongo <hongonicholas0@gmail.com>
2024-02-07 23:44:14 +0300

    a function that returns an object (Python data structure) represented by a JSON string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+import json
+"""
+
+
+import json
+"""
+a function that returns an object (Python data structure) represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """
+    a function that returns an object (Python data structure) represented by a JSON string
+    """
+    return json.loads(my_str)
