defba5ec37d9d30ddf3f3af97580d810fb0cb768
Nickhongo <hongonicholas0@gmail.com>
2024-02-13 01:24:12 +0300

    Write the first class Base:

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines the Base class."""
+
+class Base:
+    """Base class used to manage id atrr"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize Base intance with id"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
