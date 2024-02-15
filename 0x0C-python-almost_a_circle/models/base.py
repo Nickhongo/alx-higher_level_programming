0bcab23c57b88330523f0288dc6b62a8cf2dd791
Nickhongo <hongonicholas0@gmail.com>
2024-02-15 12:40:07 +0300

    raise TypeError("list_dictionaries must be a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,16 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return Json string representation"""
+        if isinstance(list_dictionaries, (list,)) or list_dictionaries is None:
+            if type(list_dictionaries) is list:
+                for _ in list_dictionaries:
+                    if not isinstance(_, dict):
+                        raise TypeError(
+                            "list_dictionaries must be a list of dictionaries"
+                        )
+        else:
+            raise TypeError("list_dictionaries must be a list of dictionaries")
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
