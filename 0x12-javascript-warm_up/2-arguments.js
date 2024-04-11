fb338f89173f2becfcf17b0305354446b74f8930
Nickhongo <hongonicholas0@gmail.com>
2024-04-11 07:03:39 +0300

    Write a script that prints the first argument passed to it:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 const numOfArgs = process.argv.length;
 
-if  (numOfArgs === 2)
-{
+if (numOfArgs === 2) {
 	console.log('No argument');
-}
-else if (numOfArgs === 3)
-{
+} else if (numOfArgs === 3) {
 	console.log('Argument found');
-
-}
-else
-{
+} else {
 	console.log('Arguments found');
 }
